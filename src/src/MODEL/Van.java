9746a1280719e88072c5b0eb0b115f463488dbc0
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-06-14 20:12:43 -0500

    Constructor y metodo toString en Automovil

--- src/src/MODEL/Van.java
+++ src/src/MODEL/Van.java
@@ -1,4 +1,19 @@
 package MODEL;
 
 public class Van extends Vehiculo{
+
+    public Van(String ruta, String placa) {
+        super(ruta, placa);
+
+        capacidadMax = 8;
+        tarifaBaja = 14000;
+        tarifaMedia = 18000;
+        tarifaAlta = 23000;
+    }
+
+    @Override
+    public String toString() {
+        return "Van{" +
+                super.toString();
+    }
 }
