c23d9cc8ee44824077eac365eafa17692bca72b9
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-06-14 20:43:55 -0500

    Feat: creacion clase Turno

---
+++ src/src/MODEL/Turno.java
@@ -0,0 +1,68 @@
+package MODEL;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Turno {
+    String idTurno;
+    Conductor conductor;
+    LocalDateTime fechaInicio;
+    LocalDateTime fechaFin;
+    boolean activo;
+
+    public Turno(String idTurno, LocalDateTime fechaInicio, LocalDateTime fechaFin, Conductor conductor) {
+        this.idTurno = idTurno;
+        this.fechaInicio = fechaInicio;
+        this.fechaFin = fechaFin;
+        this.conductor = conductor;
+    }
+
+    public boolean isActivo() {
+        return activo;
+    }
+
+    public void setActivo(boolean activo) {
+        this.activo = activo;
+    }
+
+    public String getIdTurno() {
+        return idTurno;
+    }
+
+    public void setIdTurno(String idTurno) {
+        this.idTurno = idTurno;
+    }
+
+    public LocalDateTime getFechaInicio() {
+        return fechaInicio;
+    }
+
+    public void setFechaInicio(LocalDateTime fechaInicio) {
+        this.fechaInicio = fechaInicio;
+    }
+
+    public LocalDateTime getFechaFin() {
+        return fechaFin;
+    }
+
+    public void setFechaFin(LocalDateTime fechaFin) {
+        this.fechaFin = fechaFin;
+    }
+
+    public Conductor getConductor() {
+        return conductor;
+    }
+
+    public void setConductor(Conductor conductor) {
+        this.conductor = conductor;
+    }
+
+    @Override
+    public String toString() {
+        return "id Turno='" + idTurno + '\'' +
+                ", cedulaconductor ='" + conductor.cedula + '\'' +
+                ", fecha inicio ='" + fechaInicio + '\'' +
+                ", fecha fin ='" + fechaFin + '\'' +
+                ", activo =" + activo ;
+    }
+}
