1850f78cb3415685a993a71969fc077db9f409d7
ErickFuentes67 <ejhoanfuentes@unicesar.edu.co>
2026-06-14 20:16:20 -0500

    Metodos clase abstracta Conductor

--- src/src/MODEL/Conductor.java
+++ src/src/MODEL/Conductor.java
@@ -3,8 +3,33 @@ package MODEL;
 public abstract class Conductor {
     String nombre;
     String cedula;
-    double tarifaBase;
+    String telefono;
+
+    public Conductor(String cedula, String nombre, String telefono) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+        this.telefono = telefono;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
 
     void calcularValorNeto(double tarifaBase){};
+
+    @Override
+    public String toString() {
+        return "nombre='" + nombre + '\'' +
+                ", cedula='" + cedula + '\'' +
+                ", telefono=" + telefono ;
+    }
 
 }
